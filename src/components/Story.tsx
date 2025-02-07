9c8a2ceb62677920f2cb7244c433c25296f63ee8
Tigran Hakobyan <tik.hakobian@gmail.com>
2025-02-07 14:14:44 +0400

    Update Story.tsx

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -291,8 +291,8 @@ Story.defaultProps = {
 const styles = StyleSheet.create({
   modal: {
     flex: 1,
-    height: Dimensions.get('window').height,
-    width: Dimensions.get('window').width,
+    height: Dimensions.get('screen').height,
+    width: Dimensions.get('screen').width,
   },
 });
 
